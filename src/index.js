680f541dd6933c49e82b5598ccfd32e562585cef
kims <kimi.brent08@gmail.com>
2025-04-28 15:12:19 +0800

    up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'; // Import React
 import ReactDOM from 'react-dom/client'; // Import ReactDOM for React 18+
-// If you are using React 17 or below, use `import ReactDOM from 'react-dom';`
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-// Use ReactDOM.createRoot for React 18+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
